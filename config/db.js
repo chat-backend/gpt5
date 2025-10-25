02bc21ea44ece7b7a6c934d4cb2a5fd032daebe1
TinhAnThu <tinhanthu@gmail.com>
2025-10-26 03:50:11 +0700

    Cập nhật file db.js với biến MONGO_USER và MONGO_PASS

--- config/db.js
+++ config/db.js
@@ -1,14 +1,15 @@
 // db.js (ESM version)
 import mongoose from 'mongoose';
 
-const mongoUri = process.env.MONGO_URL;
+const user = process.env.MONGO_USER;
+const pass = process.env.MONGO_PASS;
+
+// Ghép lại connection string
+const mongoUri = `mongodb+srv://${user}:${pass}@cluster0.qbg9jtn.mongodb.net/chatgpt?retryWrites=true&w=majority&appName=Cluster0`;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(mongoUri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(mongoUri);
     console.log("✅ MongoDB connected");
   } catch (err) {
     console.error("❌ Lỗi kết nối MongoDB:", err);
